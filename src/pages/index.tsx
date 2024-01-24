5ac0cffa556989dac2ad32548613f0b56be7e70f
Kanishk Khurana <kanishkkhurana152@gmail.com>
2024-01-25 01:22:25 +0530

    meta description for main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import Head from '@docusaurus/Head'; 
+
 
 export default function Home(): JSX.Element {
   const docsPath = useBaseUrl('/docs');
@@ -8,5 +10,9 @@ export default function Home(): JSX.Element {
     window.location.href = docsPath;
   }, []);
 
-  return null;
+  return (
+    <Head>
+    <meta name="description" content="Welcome to the documentation for the alpha of Fleek.xyz. Whether you are an expert or an absolute beginner, you'll find your answers here." />    
+    </Head>
+  );
 }
